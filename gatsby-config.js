fd1521b4fbc7c6cefc15ce02ab1c020bc6dd93f1
Your Name <you@example.com>
2019-03-22 19:38:33 -0600

    first commit with the starter installed and word press installed

---
+++ gatsby-config.js
@@ -0,0 +1,103 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    'gatsby-plugin-offline',
+    `gatsby-plugin-sass`,
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        /*
+         * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+         * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
+         */
+        baseUrl: "playground.purpleandbold.net",
+        // The protocol. This can be http or https.
+        protocol: "https",
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the assumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: false,
+        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
+        // This feature is untested for sites hosted on wordpress.com.
+        // Defaults to true.
+        useACF: true,
+        // Include specific ACF Option Pages that have a set post ID
+        // Regardless if an ID is set, the default options route will still be retrieved
+        // Must be using V3 of ACF to REST to include these routes
+        // Example: `["option_page_1", "option_page_2"]` will include the proper ACF option
+        // routes with the ID option_page_1 and option_page_2
+        // The IDs provided to this array should correspond to the `post_id` value when defining your
+        // options page using the provided `acf_add_options_page` method, in your WordPress setup
+        // Dashes in IDs will be converted to underscores for use in GraphQL
+        acfOptionPageIds: [],
+
+        // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
+        // It can help you debug specific API Endpoints problems.
+        verboseOutput: false,
+        // Set how many pages are retrieved per API request.
+        perPage: 100,
+        // Search and Replace Urls across WordPress content.
+        // Set how many simultaneous requests are sent at once.
+        concurrentRequests: 10,
+        // Set WP REST API routes whitelists
+        // and blacklists using glob patterns.
+        // Defaults to whitelist the routes shown
+        // in the example below.
+        // See: https://github.com/isaacs/minimatch
+        // Example:  `["/*/*/comments", "/yoast/**"]`
+        // ` will either include or exclude routes ending in `comments` and
+        // all routes that begin with `yoast` from fetch.
+        // Whitelisted routes using glob patterns
+        includedRoutes: [
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/users",
+          "**/classe",
+          "**/product"
+        ],
+        // Blacklisted routes using glob patterns
+        excludedRoutes: [],
+        // use a custom normalizer which is applied after the built-in ones.
+        normalizer: function ({ entities }) {
+          return entities
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-instagram`,
+      options: {
+        username: `amauryguichon`,
+      },
+    },
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Link } from "gatsby"
+import Img from "gatsby-image";
+import Layout from "../components/layout/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+import Content from '../components/utility/Content/Content'
+
+class IndexPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    const pics = this.props.data.allInstaNode.edges
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <Content>
+          {
+            pics.map(pic => (
+              <div>
+                <p>{pic.node.caption}</p>
+                <Img fluid={pic.node.localFile.childImageSharp.fluid} />
+              </div>
+            ))
+          }
+        </Content>
+      </Layout>
+    );
+  }
+}
+
+export const query = graphql`
+query {
+  allInstaNode {
+    edges {
+      node {
+        id
+        likes
+        comments
+        mediaType
+        preview
+        original
+        timestamp
+        caption
+        localFile {
+          childImageSharp {
+            fluid(maxWidth: 600) {
+              ...GatsbyImageSharpFluid_noBase64
+            }
+          }
+        }
+        # Only available with the public api scraper
+        thumbnails {
+          src
+          config_width
+          config_height
+        }
+        dimensions {
+          height
+          width
+        }
+      }
+    }
+  }
+}
+`;
+export default IndexPage;
