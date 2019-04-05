4b892ed7d1b1f9adeef549a548141b6e2e334a8b
cole smith <smithcole@protonmail.com>
2019-04-04 19:50:50 -0600

    bugfix

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,4 +1,4 @@
-// import React, { Component } from 'react';
+import React, { Component } from "react";
 // import { Link } from "gatsby"
 // import Img from "gatsby-image";
 // import Layout from "../components/layout/layout"
@@ -7,37 +7,38 @@
 // import Content from '../components/utility/Content/Content'
 // import ProductCard from '../components/productCard/productCard';
 
-// class Products extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {}
-//     }
-//     render() {
-//         const data = this.props.data.allWordpressWpProduct.edges
-//         return (
-//             <Layout>
-//                 <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-//                 <Content>
-//                     {
-//                         data.map(prod => (
-//                             <div
-//                                 key={prod.id}
-//                             >
-//                                 <ProductCard
-//                                     title={prod.node.title}
-//                                     wpid={prod.node.wordpress_id}
-//                                     img={prod.node.acf.main_image.localFile.childImageSharp.fluid}
-//                                     cartImg={prod.node.acf.main_image.localFile.url}
-//                                     dec={prod.node.acf.decription}
-//                                 />
-//                             </div>
-//                         ))
-//                     }
-//                 </Content>
-//             </Layout>
-//         );
-//     }
-// }
+class Products extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    //         const data = this.props.data.allWordpressWpProduct.edges
+    return (
+      <h1>hello</h1>
+      //             <Layout>
+      //                 <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      //                 <Content>
+      //                     {
+      //                         data.map(prod => (
+      //                             <div
+      //                                 key={prod.id}
+      //                             >
+      //                                 <ProductCard
+      //                                     title={prod.node.title}
+      //                                     wpid={prod.node.wordpress_id}
+      //                                     img={prod.node.acf.main_image.localFile.childImageSharp.fluid}
+      //                                     cartImg={prod.node.acf.main_image.localFile.url}
+      //                                     dec={prod.node.acf.decription}
+      //                                 />
+      //                             </div>
+      //                         ))
+      //                     }
+      //                 </Content>
+      //             </Layout>
+    );
+  }
+}
 
 // export const query = graphql`
 // query {
@@ -65,4 +66,4 @@
 //       }
 // }
 // `;
-// export default Products;
+export default Products;
