30584ec71eeef629e276ff29972b0f4a7cff3a79
Quinn <quinnsmyers@gmail.com>
2019-04-06 10:10:32 +1100

    fixed some styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import { Link } from "gatsby"
+import React, { Component } from "react";
+import { Link } from "gatsby";
 import Img from "gatsby-image";
-import Layout from "../components/layout/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import Content from '../components/utility/Content/Content'
-import BackgroundBanner from "../components/background_banner/background_banner"
-import "../components/styles/homepage.sass"
-import ButtonRound from '../components/buttonRound/buttonRound'
-import Signup from '../components/page_bottom_signup/page_signup'
-import Spacer from '../components/spacer/spacer'
+import Layout from "../components/layout/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+import Content from "../components/utility/Content/Content";
+import BackgroundBanner from "../components/background_banner/background_banner";
+import "../components/styles/homepage.sass";
+import ButtonRound from "../components/buttonRound/buttonRound";
+import Signup from "../components/page_bottom_signup/page_signup";
+import Spacer from "../components/spacer/spacer";
 
 class IndexPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
   render() {
-    const courses = this.props.data.allWordpressWpCourseWeek.edges
+    const courses = this.props.data.allWordpressWpCourseWeek.edges;
 
-    const data = this.props.data.allWordpressPage.edges[0].node
-    const program = this.props.data.programInfo.edges[0].node
-    const sponsors = this.props.data.allWordpressAcfSponsor.edges
+    const data = this.props.data.allWordpressPage.edges[0].node;
+    const program = this.props.data.programInfo.edges[0].node;
+    const sponsors = this.props.data.allWordpressAcfSponsor.edges;
 
     return (
       <Layout>
@@ -34,33 +34,37 @@ class IndexPage extends Component {
           sides={true}
           img={data.acf.banner.background_image.localFile.childImageSharp.fluid}
           heroimg={data.acf.banner.hero_image.localFile.childImageSharp.fluid}
-        >
-
-        </BackgroundBanner>
+        />
         <Content>
           <div className="wraper">
-            <Spacer></Spacer>
+            <Spacer />
             <div className="bio">
               <div className="bio__info">
                 <div className="bio__info__title">
                   <h3>{data.acf.bio_section.title}</h3>
                   <h4>{data.acf.bio_section.name} </h4>
                 </div>
-                <p dangerouslySetInnerHTML={{ __html: data.acf.bio_section.bio }}></p>
+                <p
+                  dangerouslySetInnerHTML={{ __html: data.acf.bio_section.bio }}
+                />
                 <ButtonRound
                   innerText={"Meet The Team"}
                   action={"team"}
                   type="gatsbylink"
                   passedState={""}
-                  padding='5px 30px'
-                  fsize='1'
+                  padding="4.5px 20px"
+                  fsize="0.85"
                 />
               </div>
               <div className="bio__image">
-                <Img fluid={data.acf.bio_section.image.localFile.childImageSharp.fluid} />
+                <Img
+                  fluid={
+                    data.acf.bio_section.image.localFile.childImageSharp.fluid
+                  }
+                />
               </div>
             </div>
-            <Spacer></Spacer>
+            <Spacer />
           </div>
         </Content>
         <div className="program__overview">
@@ -71,7 +75,11 @@ class IndexPage extends Component {
           </div>
           <div className="program__overview__card">
             <h4>Curriculum Overview</h4>
-            <p dangerouslySetInnerHTML={{ __html: program.acf.curriculum_overview }}></p>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: program.acf.curriculum_overview
+              }}
+            />
             <div className="program__overview__card__info">
               <div className="program__overview__card__info__data">
                 <h4>Location</h4>
@@ -83,34 +91,29 @@ class IndexPage extends Component {
               </div>
               <div className="program__overview__card__info__data">
                 <h4>Class Size</h4>
-                <p>
-                  {program.acf.class_size}
-                </p>
+                <p>{program.acf.class_size}</p>
               </div>
             </div>
-
           </div>
         </div>
         <Content>
           <div className="wraper">
-            <Spacer></Spacer>
+            <Spacer />
             <div className="sponsers">
               <h3>Sponsors</h3>
-              <p dangerouslySetInnerHTML={{ __html: data.acf.sponsor_info }}></p>
-              {sponsors.map(el =>
-                (
-                  <div className="sponsers__single" key={el.node.id}>
-                    <div className="sponsers__single__image">
-                      <Img
-                        fluid={el.node.acf.logo.localFile.childImageSharp.fluid}
-                      />
-                    </div>
+              <p dangerouslySetInnerHTML={{ __html: data.acf.sponsor_info }} />
+              {sponsors.map(el => (
+                <div className="sponsers__single" key={el.node.id}>
+                  <div className="sponsers__single__image">
+                    <Img
+                      fluid={el.node.acf.logo.localFile.childImageSharp.fluid}
+                    />
                   </div>
-                )
-              )}
+                </div>
+              ))}
             </div>
-            <Spacer></Spacer>
-            <Signup></Signup>
+            <Spacer />
+            <Signup />
           </div>
         </Content>
       </Layout>
@@ -119,90 +122,91 @@ class IndexPage extends Component {
 }
 
 export const query = graphql`
-query {
-  allWordpressPage(filter: {title: {regex: "/Home Page/"}}) {
-    edges {
-      node {
-        title
-        acf {
-          banner {
-            hero_text
-            cta
-            background_image {
+  query {
+    allWordpressPage(filter: { title: { regex: "/Home Page/" } }) {
+      edges {
+        node {
+          title
+          acf {
+            banner {
+              hero_text
+              cta
+              background_image {
+                localFile {
+                  childImageSharp {
+                    fluid(maxWidth: 600) {
+                      ...GatsbyImageSharpFluid_noBase64
+                    }
+                  }
+                }
+              }
+              hero_image {
+                localFile {
+                  childImageSharp {
+                    fluid(maxWidth: 600) {
+                      ...GatsbyImageSharpFluid_noBase64
+                    }
+                  }
+                }
+              }
+            }
+            bio_section {
+              title
+              name
+              bio
+              image {
+                localFile {
+                  childImageSharp {
+                    fluid(maxWidth: 600) {
+                      ...GatsbyImageSharpFluid_noBase64
+                    }
+                  }
+                }
+              }
+            }
+            academy_image {
+              localFile {
+                childImageSharp {
+                  fluid(maxWidth: 1000) {
+                    ...GatsbyImageSharpFluid_noBase64
+                  }
+                }
+              }
+            }
+            sponsor_info
+          }
+        }
+      }
+    }
+    programInfo: allWordpressPage(filter: { title: { regex: "/Program/" } }) {
+      edges {
+        node {
+          title
+          acf {
+            curriculum_overview
+            class_size
+          }
+        }
+      }
+    }
+    allWordpressWpCourseWeek {
+      edges {
+        node {
+          id
+        }
+      }
+    }
+    allWordpressAcfSponsor {
+      edges {
+        node {
+          id
+          acf {
+            logo {
               localFile {
                 childImageSharp {
                   fluid(maxWidth: 600) {
-                     ...GatsbyImageSharpFluid_noBase64
+                    ...GatsbyImageSharpFluid_noBase64
                   }
-                }
-              }
-            }
-            hero_image{
-               localFile {
-                childImageSharp {
-                  fluid(maxWidth: 600) {
-                     ...GatsbyImageSharpFluid_noBase64
-                  }
-                }
-              }
-            }
-          }
-          bio_section {
-            title
-            name
-            bio
-            image {
-              localFile {
-                childImageSharp {
-                  fluid(maxWidth: 600) {
-                     ...GatsbyImageSharpFluid_noBase64
-                  }
-                }
-              }
-            }
-          }
-          academy_image {
-            localFile {
-              childImageSharp {
-                fluid(maxWidth: 1000) {
-                   ...GatsbyImageSharpFluid_noBase64
-                }
-              }
-            }
-          }
-          sponsor_info
-        }
-      }
-    }
-  }
-  programInfo: allWordpressPage(filter: {title: {regex: "/Program/"}}) {
-    edges {
-      node {
-        title
-        acf {
-          curriculum_overview
-          class_size
-        }
-      }
-    }
-  }
-  allWordpressWpCourseWeek {
-    edges {
-      node {
-        id
-      }
-    }
-  }
-  allWordpressAcfSponsor {
-    edges {
-      node {
-        id
-        acf {
-          logo {
-            localFile {
-              childImageSharp {
-                fluid(maxWidth: 600) {
-                  ...GatsbyImageSharpFluid_noBase64
                 }
               }
             }
@@ -211,6 +215,5 @@ query {
       }
     }
   }
-}
 `;
 export default IndexPage;
