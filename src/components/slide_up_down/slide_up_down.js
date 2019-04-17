9acbb35903b80e51007c5ea8d08ae554231db86e
Quinn <quinnsmyers@gmail.com>
2019-04-16 22:35:05 -0700

    added bio drop down feature

--- src/components/slide_up_down/slide_up_down.js
+++ src/components/slide_up_down/slide_up_down.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 //slide up slide down plugin
 import { SlideDown } from "react-slidedown";
 import "react-slidedown/lib/slidedown.css";
+import PropTypes from "prop-types";
 
 //images
 import arrowIcon from "../../images/arrow-icon-services.svg";
@@ -17,7 +18,7 @@ class SlideUpDown extends Component {
         "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMCA3LjMzbDIuODI5LTIuODMgOS4xNzUgOS4zMzkgOS4xNjctOS4zMzkgMi44MjkgMi44My0xMS45OTYgMTIuMTd6Ii8+PC9zdmc+"
     };
   }
-  componentDidUpdate() {}
+  // componentDidUpdate() {}
   toggleSlide() {
     this.setState({ open: !this.state.open });
   }
@@ -31,6 +32,9 @@ class SlideUpDown extends Component {
   //       action: 'User clicked on Service Item',
   //     })
   // }
+  nameOfExpanded() {
+    return this.name;
+  }
   render() {
     return (
       <div className="faq__section__right__slide">
@@ -38,7 +42,23 @@ class SlideUpDown extends Component {
           className="faq__section__right__slide__title"
           onClick={this.handleClick.bind(this)}
         >
-          <h4>{this.props.name}</h4>
+          {/* <h4>
+            {!this.state.open ? this.props.name : this.props.expandedName}
+          </h4> */}
+          <h4
+            className={`faq__section__right__slide__title__name ${
+              !this.state.open ? "visible" : "hidden"
+            }`}
+          >
+            {this.props.name}
+          </h4>
+          <h4
+            className={`faq__section__right__slide__title__name ${
+              this.state.open ? "visible" : "hidden"
+            }`}
+          >
+            {this.props.expandedName}
+          </h4>
           <svg
             className={this.state.open ? "expanded" : ""}
             viewBox="0 0 117 63"
