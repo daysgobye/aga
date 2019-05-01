fe596115b3de4b74f5fca00e273676f05f00a702
cole smith <smithcole@protonmail.com>
2019-05-01 00:53:04 -0600

    added page gen for products and started template with query for product page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,44 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  const productTemplate = path.resolve(`src/templates/product.js`)
+  // Query for markdown nodes to use in creating pages.
+  // You can query for whatever data you want to create pages for e.g.
+  // products, portfolio items, landing pages, etc.
+  return graphql(`
+    {
+    allWordpressAcfProduct{
+   	 edges{
+      node{
+        wordpress_id
+      }
+    }
+  }
+	}
+  `).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    // Create product pages.
+    result.data.allWordpressAcfProduct.edges.forEach(edge => {
+      createPage({
+        // Path for this page — required
+        path: `/store/${edge.node.wordpress_id}`,
+        component: productTemplate,
+        context: {
+								slug: edge.node.wordpress_id
+          // Add optional context data to be inserted
+          // as props into the page component..
+          //
+          // The context data can also be used as
+          // arguments to the page GraphQL query.
+          //
+          // The page "path" is always available as a GraphQL
+          // argument.
+        },
+      })
+    })
+  })
+}
---
+++ src/templates/product.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import { Link, graphql } from "gatsby";
+import Img from "gatsby-image";
+import Layout from "../components/layout/layout"
+import Content from "../components/utility/Content/Content"
+class Product extends React.Component {
+constructor(props) {
+	super(props);
+this.state = {
+}
+
+}
+
+render() {
+				const data = this.props.data.wordpressAcfProduct
+				console.log(data)
+		return (
+						<Layout>
+						<Content>
+						good shit
+						</Content>
+						</Layout>
+		
+			
+		);
+	}
+}
+export const query = graphql`
+query($slug: Int){
+  wordpressAcfProduct(wordpress_id:{eq: $slug}) {
+        wordpress_id
+        acf {
+          main_image {
+            localFile {
+              childImageSharp {
+                fluid {
+                  src
+                }
+              }
+            }
+          }
+          more_images {
+            localFile {
+              childImageSharp {
+                fluid {
+                  src
+                }
+              }
+            }
+          }
+          product_description
+          in_stock
+          show_in_store
+          price
+          variable_product
+					colors{
+            color
+          }
+          sizes{
+            size
+
+          }
+        }
+      }
+  }
+
+`
+ export default Product ;
